515d32f07175c8adb697bfd4af0cf820e8d74eb7
Daniel Luiz <danielluizthiessen@gmail.com>
2021-05-14 17:17:09 -0300

    Fiz o layout da primeira rota

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,44 @@
-export default function App (){
-    return();
+export default function App() {
+    return (
+
+        <div className="fundo">
+            <div className="topo">CINEFLEX</div>
+            <div className="conteudo">
+                <div className="tituloRota">Selecione o filme</div>
+                <div className="containerFilmes">
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                    <div className="caixa">
+                        <img src="imgs/2067.png" className="poster" />
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+
+    );
 }
